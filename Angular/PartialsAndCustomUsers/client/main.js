75b65e2a276a9fad2958c391a270b77ce86438a8
Steve Duncan <SteveDuncan@comcast.net>
2016-10-06 12:32:35 -0700

    Added PartialsAndCustomUsers exercise to Angular

---
+++ Angular/PartialsAndCustomUsers/client/main.js
@@ -0,0 +1,74 @@
+var app = angular.module('usersApp',['ngRoute']);
+
+//configure routes
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl: 'partials/listUsers.html'
+		})
+		.when('/customizeUsers',{
+			templateUrl: 'partials/customizeUsers.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+	});
+});
+
+
+
+//create factory and attach to module
+app.factory('userFactory',['$http', function($http){
+	var factory = {};
+	var users = [];
+
+	factory.index = function(callback){
+		callback(users);
+	}
+
+	factory.getUsers = function(callback){
+		callback(users);
+	}
+
+	factory.create = function(newUser, callback){
+		users.push(newUser);
+		callback(users);
+	}
+	factory.delete = function(index, callback){
+		users.splice(index, 1);
+		callback(users);
+	}
+	
+	
+	return factory;
+}]);
+
+
+//create users controller and attach to module
+app.controller('UsersController', ['$scope', 'userFactory', function ($scope, userFactory){
+	
+	function getUsers(data){
+		$scope.users = data;
+		$scope.newUser = {};
+	}
+
+
+	$scope.createUser = function(){
+		userFactory.create($scope.newUser, getUsers);
+		$scope.newUser = {};
+	}
+
+	$scope.deleteUser = function(index){
+		userFactory.delete(index, getUsers);
+	}
+
+}]);
+
+//create list controller and attach to module
+app.controller('ListUsersController', ['$scope', 'userFactory',  function ($scope, userFactory){
+	function getUsers(data){
+		$scope.users = data;
+		$scope.newUser = {};
+	}
+	userFactory.index(getUsers);
+
+}])
